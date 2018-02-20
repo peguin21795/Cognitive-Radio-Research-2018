44093ac38d8e24a08ba5420cecb6215e1613ff07
Andy <awang@mymail.mines.edu>
2018-02-20 02:53:06 -0700

    Updated C++ code

--- Research/Research/main.cpp
+++ Research/Research/main.cpp
@@ -9,9 +9,14 @@ int main()
 	struct bladerf_devinfo dev_info;
 	bladerf_init_devinfo(&dev_info);
 	int status = bladerf_open_with_devinfo(&dev, &dev_info); 
-	cout << "Is FPGA configured? ";
-	cout << bladerf_is_fpga_configured(dev) << endl;
-	bladerf_close(dev);
-	system("PAUSE");
-	return 0;
+	if (status != 0)
+	{
+		cout << "Error! Connection to the bladeRF failed." << endl;
+		return -1;
+	}
+	else
+	{
+		bladerf_close(dev);
+		return 0;
+	}
 }
