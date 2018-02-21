f83d1af8c813e08bdd8948aafbb97089180b78cf
Andy <awang@mymail.mines.edu>
2018-02-21 01:32:02 -0700

    Additional functionalities completed

--- Research/Research/main.cpp
+++ Research/Research/main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 #define SELECT_LEN 4
 
-bool testBladeRF(bladerf *bf, bladerf_devinfo dbf)
+bool testBladeRF(bladerf **bf, bladerf_devinfo &dbf)
 {
-	int status = bladerf_open_with_devinfo(&bf, &dbf);
+	int status = bladerf_open_with_devinfo(bf, &dbf);
 	if (status != 0)
 	{
 		return 0;
@@ -21,7 +21,68 @@ bool testBladeRF(bladerf *bf, bladerf_devinfo dbf)
 	}
 }
 
-void menuBladeRF()
+void acquireData(bladerf **bf)
+{
+	cout << "Verification: " << &*bf << endl;
+
+	bladerf_enable_module(*bf, BLADERF_MODULE_RX, true);
+	bladerf_enable_module(*bf, BLADERF_MODULE_TX, false);
+	bladerf_set_tuning_mode(*bf, BLADERF_TUNING_MODE_FPGA);
+	bladerf_set_sampling(*bf, BLADERF_SAMPLING_INTERNAL);
+	unsigned int specifyFrequency;
+	while (true)
+	{
+		cout << "Specify frequency: ";
+		cin >> specifyFrequency;
+		if (specifyFrequency >= 300000000u && specifyFrequency <= 3800000000)
+		{
+			bladerf_set_frequency(*bf, BLADERF_MODULE_RX, specifyFrequency);
+			break;
+		}
+		else
+		{
+			cout << "Invalid frequency!" << endl;
+		}
+	}
+	// For now, we will set the sampling rate to be exactly 24
+	unsigned int samplingRate = 20000000u;
+	bladerf_set_sample_rate(*bf, BLADERF_MODULE_RX, samplingRate, NULL);
+	unsigned int bandwidth = samplingRate / 2;
+	bladerf_set_bandwidth(*bf, BLADERF_MODULE_RX, bandwidth, NULL);
+
+	// Setting up the interface to actually receive the data
+	bladerf_sync_config(*bf, BLADERF_MODULE_RX, BLADERF_FORMAT_SC16_Q11, 16, 8192, 8, 3500);
+	bool completion = false;
+
+	// Code partially borrowed from Nuand for the purpose of understanding the process
+
+
+	bladerf_enable_module(*bf, BLADERF_MODULE_RX, false);
+	return;
+}
+
+void createOutputTxt(bladerf **bf)
+{
+	ofstream ofile;
+	return;
+}
+
+void modifySettings(bladerf **bf)
+{
+	int gain;
+	unsigned int frequency;
+	bladerf_get_rxvga1(*bf, &gain);
+	bladerf_get_frequency(*bf, BLADERF_MODULE_RX, &frequency);
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "                    Current Properties Configuration                          " << endl;
+	cout << "------------------------------------------------------------------------------" << endl;
+	cout << "Gain: " << gain << endl;
+	cout << "Frequency: " << frequency << " Hz" << endl;
+	cout << endl;
+	return;
+}
+
+void menuBladeRF(bladerf **bf)
 {
 	int input;
 	bool quitStatus = false;
@@ -41,7 +102,7 @@ void menuBladeRF()
 		{
 			case 0:
 			{
-				cout << 0 << endl;
+				acquireData(bf);
 				break;
 			}
 			case 1:
@@ -51,7 +112,7 @@ void menuBladeRF()
 			}
 			case 2:
 			{
-				cout << 2 << endl;
+				modifySettings(bf);
 				break;
 			}
 			case 3:
@@ -73,22 +134,6 @@ void menuBladeRF()
 	return;
 }
 
-void acquireData()
-{
-	return;
-}
-
-void createOutputTxt()
-{
-	ofstream ofile;
-	return;
-}
-
-void modifySettings()
-{
-	return;
-}
-
 int main()
 {
 	// Testing the bladeRF x40 to make sure that it is successfully connected to the host PC.
@@ -98,9 +143,10 @@ int main()
 
 	bladerf_init_devinfo(&dev_info);
 	
-	if (testBladeRF(dev, dev_info))
+	if (testBladeRF(&dev, dev_info))
 	{
-		menuBladeRF();
+		cout << &dev << endl;
+		menuBladeRF(&dev);
 		EXIT_CODE = 0;
 	}
 	else
