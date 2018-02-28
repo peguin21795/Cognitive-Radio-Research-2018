4c2ab87b9d4e7383b689743f3d310fdf87465530
Andy <awang@mymail.mines.edu>
2018-02-27 22:43:04 -0700

    IQ data can now be generated. Also added ability to create output file that can be used in a MATLAB script

--- Research/Research/main.cpp
+++ Research/Research/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdint>
+#include <iterator>
 #include <vector>
 #include <fstream>
 #include <string>
@@ -7,6 +9,17 @@
 using namespace std;
 
 #define SELECT_LEN 4
+
+struct IQ
+{
+	int16_t inPhase;
+	int16_t quadrature;
+};
+
+void printIQ(IQ iq)
+{
+	cout << "(" << iq.inPhase << ", " << iq.quadrature << ")" << endl;
+}
 
 bool testBladeRF(bladerf **bf, bladerf_devinfo &dbf)
 {
@@ -21,20 +34,25 @@ bool testBladeRF(bladerf **bf, bladerf_devinfo &dbf)
 	}
 }
 
-void acquireData(bladerf **bf)
+vector <IQ> acquireData(bladerf **bf)
 {
 	cout << "Verification: " << &*bf << endl;
-
+	vector <IQ> rawIQ;
 	bladerf_enable_module(*bf, BLADERF_MODULE_RX, true);
 	bladerf_enable_module(*bf, BLADERF_MODULE_TX, false);
-	bladerf_set_tuning_mode(*bf, BLADERF_TUNING_MODE_FPGA);
 	bladerf_set_sampling(*bf, BLADERF_SAMPLING_INTERNAL);
+	bladerf_set_tuning_mode(*bf, BLADERF_TUNING_MODE_FPGA);
 	unsigned int specifyFrequency;
 	while (true)
 	{
-		cout << "Specify frequency: ";
+		cout << "Specify frequency (Enter -1 for default frequency value): ";
 		cin >> specifyFrequency;
-		if (specifyFrequency >= 300000000u && specifyFrequency <= 3800000000)
+		if (specifyFrequency == -1)
+		{
+			bladerf_get_frequency(*bf, BLADERF_MODULE_RX, &specifyFrequency);
+			break;
+		}
+		else if (specifyFrequency >= 300000000 && specifyFrequency <= 3800000000)
 		{
 			bladerf_set_frequency(*bf, BLADERF_MODULE_RX, specifyFrequency);
 			break;
@@ -44,26 +62,51 @@ void acquireData(bladerf **bf)
 			cout << "Invalid frequency!" << endl;
 		}
 	}
+	cout << "Frequency: " << specifyFrequency << endl;
 	// For now, we will set the sampling rate to be exactly 24
-	unsigned int samplingRate = 20000000u;
+	unsigned int samplingRate = 30000000;
 	bladerf_set_sample_rate(*bf, BLADERF_MODULE_RX, samplingRate, NULL);
 	unsigned int bandwidth = samplingRate / 2;
 	bladerf_set_bandwidth(*bf, BLADERF_MODULE_RX, bandwidth, NULL);
 
 	// Setting up the interface to actually receive the data
-	bladerf_sync_config(*bf, BLADERF_MODULE_RX, BLADERF_FORMAT_SC16_Q11, 16, 8192, 8, 3500);
-	bool completion = false;
+	bladerf_sync_config(*bf, BLADERF_MODULE_RX, BLADERF_FORMAT_SC16_Q11, 16, 8192, 8, 1000);
+	const int sampleLength = 10000;
+	int16_t* rx_samples = new int16_t [2 * sampleLength];
 
 	// Code partially borrowed from Nuand for the purpose of understanding the process
-
-
+	bladerf_sync_rx(*bf, rx_samples, sampleLength, NULL, 1000);
 	bladerf_enable_module(*bf, BLADERF_MODULE_RX, false);
-	return;
+	IQ tempIQ;
+
+	for (int i = 0; i < 2 * sampleLength; i++)
+	{
+		tempIQ.inPhase = rx_samples[i];
+		tempIQ.quadrature = rx_samples[i + 1];
+		rawIQ.push_back(tempIQ);
+	}
+	delete[] rx_samples;
+	return rawIQ;
 }
 
-void createOutputTxt(bladerf **bf)
+void createOutputTxt(vector <IQ> rawIQ, bladerf** bf)
 {
+	if (rawIQ.size() == 0)
+	{
+		cerr << "Error: You must first acquire data from the bladeRF before proceeding with data generation!" << endl;
+		return;
+	}
 	ofstream ofile;
+	unsigned int frequencyUsed;
+
+	ofile.open("rawIQ.txt");
+	ofile << "Raw IQ sampling" << endl;
+	for (int i = 0; i < rawIQ.size(); i++)
+	{
+		ofile << rawIQ[i].inPhase << " " << rawIQ[i].inPhase << "\n";
+	}													
+	cout << "File successfully created! All elements have been successfully erased" << endl;
+	ofile.close();
 	return;
 }
 
@@ -86,7 +129,7 @@ void menuBladeRF(bladerf **bf)
 {
 	int input;
 	bool quitStatus = false;
-	vector <double> rawIQ;
+	vector <IQ> rawIQ;
 
 	string SELECTION[SELECT_LEN] = {"Acquire Data through RX", "Create Output Text File", "Modify Settings", "Quit"};
 	cout << "Make a selection" << endl;
@@ -102,12 +145,12 @@ void menuBladeRF(bladerf **bf)
 		{
 			case 0:
 			{
-				acquireData(bf);
+				rawIQ = acquireData(bf);
 				break;
 			}
 			case 1:
 			{
-				cout << 1 << endl;
+				createOutputTxt(rawIQ, bf);
 				break;
 			}
 			case 2:
